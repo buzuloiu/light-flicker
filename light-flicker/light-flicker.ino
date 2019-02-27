c29e160102ccda18ec9bf576a715b915871a94f2
Paul Buzuloiu <pbuzu025@uottawa.ca>
2019-02-27 17:01:27 -0500

    order

--- light-flicker/light-flicker.ino
+++ light-flicker/light-flicker.ino
@@ -76,18 +76,7 @@ void loop() {
     Serial.println(text);
   }
 
-  /******************************************************************************
-                                servo code
-  *******************************************************************************/
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
+
 
   /******************************************************************************
                                 button code
@@ -109,4 +98,17 @@ void loop() {
   digitalWrite(outPin, state);
 
   previous = reading;
+
+  /******************************************************************************
+                                servo code
+  *******************************************************************************/
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
 }
